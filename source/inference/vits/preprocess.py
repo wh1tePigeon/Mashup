6ae9641726568de864ce190cfff25136b921bb7c
GYO <nickizbig@gmail.com>
2024-05-09 18:54:50 +0300

    upd

--- source/inference/vits/preprocess.py
+++ source/inference/vits/preprocess.py
@@ -1,12 +1,9 @@
 import os
 import sys
 import numpy as np
-import argparse
 import torch
 import librosa
 import torch
-import torchaudio
-import requests
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent.parent))
 from source.model.hubert.hubert import hubert_soft
@@ -32,23 +29,17 @@ def get_ppg(cfg):
         raise KeyError
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = whisper.load_model(name=m, download_root=cfg["checkpoint_path"]).to(device)
-
-    # del model.decoder
-    # cut = len(model.encoder.blocks) // 4
-    # cut = -1 * cut
-    # del model.encoder.blocks[cut:]
     model.eval()
 
     audio = whisper.load_audio(filepath)
-
     audln = audio.shape[0]
     ppg_a = []
     idx_s = 0
     while (idx_s + 15 * 16000 < audln):
         short = audio[idx_s:idx_s + 15 * 16000]
+        short = whisper.pad_or_trim(short)
         idx_s = idx_s + 15 * 16000
         ppgln = 15 * 16000 // 320
-        # short = pad_or_trim(short)
         mel = whisper.log_mel_spectrogram(short).to(device)
         if not (device == "cpu"):
             mel = mel.half()
@@ -61,7 +52,7 @@ def get_ppg(cfg):
     if (idx_s < audln):
         short = audio[idx_s:audln]
         ppgln = (audln - idx_s) // 320
-        # short = pad_or_trim(short)
+        short = whisper.pad_or_trim(short)
         mel = whisper.log_mel_spectrogram(short).to(device)
         if not (device == "cpu"):
             mel = mel.half()
@@ -70,27 +61,7 @@ def get_ppg(cfg):
             ppg = model.embed_audio(mel.unsqueeze(0)).squeeze().data.cpu().float().numpy()
             ppg = ppg[:ppgln,]  # [length, dim=1024]
             ppg_a.extend(ppg)
-    np.save(ppg_save_path, ppg, allow_pickle=False)
-
-
-
-
-
-    # mel = whisper.log_mel_spectrogram(audio).to(device)
-    # with torch.no_grad():
-    #     mel = mel + torch.randn_like(mel) * 0.1
-    #     mel = mel.unsqueeze(0)
-    #     #ppg = model.encoder(mel).squeeze().data.cpu().float().numpy()
-    #     ppg = model.embed_audio(mel)
-    #     np.save(ppg_save_path, ppg, allow_pickle=False)
-
-    # hubert = hubert_soft(cfg["checkpoint_path"])
-    # audio, _ = librosa.load(filepath, sr=16000)
-    # audio = torch.from_numpy(audio).to(device)
-    # audio = audio[None, None, :]
-    # with torch.inference_mode():
-    #     units = hubert.units(audio).squeeze().data.cpu().float().numpy()
-    #     np.save(vec_save_path, units, allow_pickle=False)
+    np.save(ppg_save_path, ppg_a, allow_pickle=False)
 
     return ppg_save_path
 
@@ -136,8 +107,7 @@ def get_pitch(cfg):
     return pitch_save_path
 
 
-
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # cfg = {
     #     "filepath" : "/home/comp/Рабочий стол/Mashup/input/ramm_test.wav",
     #     "output_dir" : "/home/comp/Рабочий стол/Mashup/output/hubert",
@@ -149,10 +119,10 @@ if __name__ == "__main__":
     #     "output_dir" : "/home/comp/Рабочий стол/Mashup/output/pitch"
     # }
 
-    cfg = {
-        "filepath" : "/home/comp/Рабочий стол/Mashup/input/ramm_test.wav",
-        "output_dir" : "/home/comp/Рабочий стол/Mashup/output/whisper",
-        "checkpoint_path" : "/home/comp/Рабочий стол/Mashup/checkpoints/whisper",
-        "model" : "small"
-    }
-    get_ppg(cfg)
+    # cfg = {
+    #     "filepath" : "/home/comp/Рабочий стол/Mashup/input/ramm_test_vocal_short.wav",
+    #     "output_dir" : "/home/comp/Рабочий стол/Mashup/output/whisper",
+    #     "checkpoint_path" : "/home/comp/Рабочий стол/Mashup/checkpoints/whisper",
+    #     "model" : "small"
+    # }
+    # get_ppg(cfg)
