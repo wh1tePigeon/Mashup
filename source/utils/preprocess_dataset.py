d8b2dc3b4518d1afb7cd1de66414b2a8e5047e1b
GYO <nickizbig@gmail.com>
2024-05-22 23:46:07 +0300

    upd

--- source/utils/preprocess_dataset.py
+++ source/utils/preprocess_dataset.py
@@ -2,23 +2,27 @@ import torch
 import torchaudio as ta
 import torch.nn.functional as F
 import os
+import sys
+from pathlib import Path
 from typing import Tuple
 import pandas as pd
-from moviepy.editor import VideoFileClip, AudioFileClip
-from pydub import AudioSegment
 from tqdm import tqdm
 import librosa
 import pyworld
 import numpy as np
 import torchcrepe
-from whisper import Whisper, ModelDimensions
+from whisper import Whisper, ModelDimensions, pad_or_trim, log_mel_spectrogram
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+from source.model.hubert.hubert import hubert_soft
 
 
 def process_dir_pitch(speaker_dirpath, output_dir, use_pyworld=True, use_crepe=False):
     assert os.path.exists(speaker_dirpath)
     assert use_pyworld ^ use_crepe
 
-    for filename in os.listdir(speaker_dirpath):
+    speakername = os.path.basename(os.path.normpath(speaker_dirpath))
+
+    for filename in tqdm(os.listdir(speaker_dirpath), desc=f'Extracting pitch for speaker {speakername}'):
         if filename.endswith(".wav"):
             device = "cuda" if torch.cuda.is_available() else "cpu"
             filepath = os.path.join(speaker_dirpath, filename)
@@ -69,7 +73,6 @@ def process_dir_pitch(speaker_dirpath, output_dir, use_pyworld=True, use_crepe=F
             np.save(savepath, f0, allow_pickle=False)
 
 
-
 def process_dir_whisper(speaker_dirpath, output_dir, model, checkpoint_path):
     assert os.path.exists(speaker_dirpath)
 
@@ -77,6 +80,7 @@ def process_dir_whisper(speaker_dirpath, output_dir, model, checkpoint_path):
                     "tiny.en", "base.en", "small.en", "medium.en"]:
         raise KeyError
     
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     checkpoint = torch.load(checkpoint_path, map_location="cpu")
     dims = ModelDimensions(**checkpoint["dims"])
     print(dims)
@@ -87,10 +91,43 @@ def process_dir_whisper(speaker_dirpath, output_dir, model, checkpoint_path):
     del model.encoder.blocks[cut:]
     model.load_state_dict(checkpoint["model_state_dict"], strict=False)
     model.eval()
-    model.half()
+    if not (device == "cpu"):
+        model.half()
     model.to(device)
     
-    for filename in os.listdir(speaker_dirpath):
+    speakername = os.path.basename(os.path.normpath(speaker_dirpath))
+
+    for filename in tqdm(os.listdir(speaker_dirpath), desc=f'Extracting ppg for speaker {speakername}'):
+        if filename.endswith(".wav"):
+            filepath = os.path.join(speaker_dirpath, filename)
+
+            audio, sr = librosa.load(filepath, sr=16000)
+            assert sr == 16000
+
+            audln = audio.shape[0]
+            ppgln = audln // 320
+            audio = pad_or_trim(audio)
+            mel = log_mel_spectrogram(audio).to(device)
+            if not (device == "cpu"):
+                mel = mel.half()
+            with torch.no_grad():
+                ppg = model.encoder(mel.unsqueeze(0)).squeeze().data.cpu().float().numpy()
+                ppg = ppg[:ppgln,]  # [length, dim=1280]
+                
+                filename = filename[:-4]
+                savepath = os.path.join(output_dir, (filename + "_ppg"))
+                np.save(savepath, ppg, allow_pickle=False)
+
+
+
+def process_dir_hubert(speaker_dirpath, output_dir, checkpoint_path):
+    assert os.path.exists(speaker_dirpath)
+
+    hubert = hubert_soft(checkpoint_path)
+
+    speakername = os.path.basename(os.path.normpath(speaker_dirpath))
+
+    for filename in tqdm(os.listdir(speaker_dirpath), desc=f'Extracting vec for speaker {speakername}'):
         if filename.endswith(".wav"):
             device = "cuda" if torch.cuda.is_available() else "cpu"
             filepath = os.path.join(speaker_dirpath, filename)
@@ -98,26 +135,108 @@ def process_dir_whisper(speaker_dirpath, output_dir, model, checkpoint_path):
             audio, sr = librosa.load(filepath, sr=16000)
             assert sr == 16000
 
+            audio = torch.from_numpy(audio).to(device)
+            audio = audio[None, None, :]
+            with torch.inference_mode():
+                units = hubert.units(audio).squeeze().data.cpu().float().numpy()
+                filename = filename[:-4]
+                savepath = os.path.join(output_dir, (filename + "_vec"))
+                np.save(savepath, units, allow_pickle=False)
 
 
-
-def process_dir_hubert(speaker_dirpath, output_dir):
-    assert os.path.exists(speaker_dirpath)
-    
 def process_dir_spk(speaker_dirpath, output_dir):
     assert os.path.exists(speaker_dirpath)
 
+
 def process_dir_spk_average(speaker_dirpath, output_dir):
     assert os.path.exists(speaker_dirpath)
-    
-def process_dir_sr32k(speaker_dirpath, output_dir):
-    assert os.path.exists(speaker_dirpath)
-    
-def process_dir_specs(speaker_dirpath, output_dir):
-    assert os.path.exists(speaker_dirpath)
+
+    speakername = os.path.basename(os.path.normpath(speaker_dirpath))
+
+    count = 0
+    average = 0
+
+    for filename in tqdm(os.listdir(speaker_dirpath), desc=f'Computing average embd for speaker {speakername}'):
+        if filename.endswith(".npy"):
+            filepath = os.path.join(speaker_dirpath, filename)
+
+            source_embed = np.load(filepath)
+            source_embed = source_embed.astype(np.float32)
+            average = average + source_embed
+            count = count + 1
+
+    if count > 0:
+        average = average / count
+        savepath = os.path.join(output_dir, speakername )
+        np.save(savepath, average, allow_pickle=False)
     
 
-def process_dir(dirpath, output_dir):
+def process_dir_sr32k(speaker_dirpath, output_dir):
+    assert os.path.exists(speaker_dirpath)
+
+    speakername = os.path.basename(os.path.normpath(speaker_dirpath))
+
+    for filename in tqdm(os.listdir(speaker_dirpath), desc=f'Resampling to 32 kHz for speaker {speakername}'):
+        if filename.endswith(".wav"):
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            filepath = os.path.join(speaker_dirpath, filename)
+
+            audio, sr = librosa.load(filepath, sr=32000)
+            audio = torch.tensor(np.copy(audio))[None]
+
+            filename = filename[:-4]
+            savepath = os.path.join(output_dir, (filename + "_sr32k.wav"))
+            ta.save(savepath, audio, sr)
+
+    
+def process_dir_specs(speaker_dirpath, output_dir, spec_cfg):
+    assert os.path.exists(speaker_dirpath)
+
+    speakername = os.path.basename(os.path.normpath(speaker_dirpath))
+
+    for filename in tqdm(os.listdir(speaker_dirpath), desc=f'Computing specs for speaker {speakername}'):
+        if filename.endswith(".wav"):
+            #device = "cuda" if torch.cuda.is_available() else "cpu"
+            filepath = os.path.join(speaker_dirpath, filename)
+
+            audio, _ = librosa.load(filepath, sr=spec_cfg["sr"])
+            audio = torch.tensor(np.copy(audio))[None]
+            audio_norm = audio / spec_cfg["max_wav_value"]
+            n_fft = spec_cfg["filter_length"]
+            hop_size = spec_cfg["hop_length"]
+            win_size = spec_cfg["win_length"]
+
+            hann_window = torch.hann_window(win_size).to(dtype=audio_norm.dtype, device=audio_norm.device)
+
+            audio_norm = torch.nn.functional.pad(
+                audio_norm.unsqueeze(1),
+                (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)),
+                mode="reflect",
+            )
+            audio_norm = audio_norm.squeeze(1)
+
+            spec = torch.stft(
+                audio_norm,
+                n_fft,
+                hop_length=hop_size,
+                win_length=win_size,
+                window=hann_window,
+                center=False,
+                pad_mode="reflect",
+                normalized=False,
+                onesided=True,
+                return_complex=False,
+            )
+
+            spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-6)
+            spec = torch.squeeze(spec, 0)
+
+            filename = filename[:-4]
+            savepath = os.path.join(output_dir, (filename + "_spec.pt"))
+            torch.save(spec, savepath)
+    
+
+def process_dir(dirpath, output_dir, model_cfgs):
     assert os.path.exists(dirpath)
 
     pitches_path = os.path.join(output_dir, "pitch")
@@ -147,13 +266,13 @@ def process_dir(dirpath, output_dir):
             os.makedirs(spk_average_savepath, exist_ok=True)
             os.makedirs(sr32k_savepaths, exist_ok=True)
 
-            process_dir_pitch(speakerdir, pit_savepath)
-            process_dir_whisper(speakerdir, whisper_savepath)
-            process_dir_hubert(speakerdir, hubert_savepath)
+            #process_dir_pitch(speakerdir, pit_savepath)
+            #process_dir_whisper(speakerdir, whisper_savepath, **model_cfgs["whisper"])
+            #process_dir_hubert(speakerdir, hubert_savepath, **model_cfgs["hubert"])
             process_dir_spk(speakerdir, spk_savepath)
-            process_dir_spk_average(spk_savepath, spk_average_savepath)
-            process_dir_sr32k(speakerdir, sr32k_savepaths)
-            process_dir_specs(speakerdir, spec_savepath)
+            #process_dir_spk_average(spk_savepath, spk_average_savepath)
+            #process_dir_sr32k(speakerdir, sr32k_savepaths)
+            #process_dir_specs(sr32k_savepaths, spec_savepath, model_cfgs["spec"])
 
 
             # for filename in os.listdir(speakerdir):
@@ -171,6 +290,33 @@ def process_dir(dirpath, output_dir):
 if __name__ == "__main__":
     cfg = {
         "dirpath" : "/home/comp/Рабочий стол/dataset/",
-        "output_dir" : "/home/comp/Рабочий стол/Mashup/output/dataset"
+        "output_dir" : "/home/comp/Рабочий стол/Mashup/output/dataset",
+        "model_cfgs" : {
+            "whisper": {
+                "model" : "small",
+                "checkpoint_path" : "/home/comp/Рабочий стол/Mashup/checkpoints/whisper/small.pt"
+            },
+            "hubert" : {
+                "checkpoint_path" : "/home/comp/Рабочий стол/Mashup/checkpoints/hubert/hubert-soft-0d54a1f4.pt"
+            },
+            "spec" : {
+                "sr" : 32000,
+                "max_wav_value" : 32768.0,
+                "filter_length" : 1024,
+                "hop_length" : 320,
+                "win_length" : 1024 
+            }
+        }
     }
+
     process_dir(**cfg)
+
+    # path1 = "/home/comp/Рабочий стол/Mashup/output/dataset/whisper/RR/Roman_Romanovič_49144_ppg.npy"
+    # path2 = "/home/comp/Загрузки/Roman_Romanovič_49144.ppg.npy"
+
+    # arr1 = np.load(path1)
+    # arr2 = np.load(path2)
+
+    # print(arr1)
+    # print("----------------")
+    # print(arr2)
