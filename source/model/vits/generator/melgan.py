ae4fc97221633ec688b23ac4d475a4969e8e2e6a
gyo <nickizbig@gmail.com>
2024-05-28 07:35:31 +0300

    upd

--- source/model/vits/generator/melgan.py
+++ source/model/vits/generator/melgan.py
@@ -7,42 +7,10 @@ from torch.nn.utils import weight_norm
 
 from torch.nn.utils import weight_norm
 
-
-class SpeakerAdapter(nn.Module):
-
-    def __init__(self,
-                 speaker_dim,
-                 adapter_dim,
-                 epsilon=1e-5
-                 ):
-        super(SpeakerAdapter, self).__init__()
-        self.speaker_dim = speaker_dim
-        self.adapter_dim = adapter_dim
-        self.epsilon = epsilon
-        self.W_scale = nn.Linear(self.speaker_dim, self.adapter_dim)
-        self.W_bias = nn.Linear(self.speaker_dim, self.adapter_dim)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        torch.nn.init.constant_(self.W_scale.weight, 0.0)
-        torch.nn.init.constant_(self.W_scale.bias, 1.0)
-        torch.nn.init.constant_(self.W_bias.weight, 0.0)
-        torch.nn.init.constant_(self.W_bias.bias, 0.0)
-
-    def forward(self, x, speaker_embedding):
-        x = x.transpose(1, -1)
-        mean = x.mean(dim=-1, keepdim=True)
-        var = ((x - mean) ** 2).mean(dim=-1, keepdim=True)
-        std = (var + self.epsilon).sqrt()
-        y = (x - mean) / std
-        scale = self.W_scale(speaker_embedding)
-        bias = self.W_bias(speaker_embedding)
-        y *= scale.unsqueeze(1)
-        y += bias.unsqueeze(1)
-        y = y.transpose(1, -1)
-        return y
-    
-
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent.parent))
+from source.model.vits.modules.speaker_adapter import SpeakerAdapter
 
 def weights_init(m):
     classname = m.__class__.__name__
